241d68a2f12ea1b9fe2a08c913ddf588112b48d2
MarcelloVSilva <marcellovcs@gmail.com>
2018-09-26 01:28:37 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import configureStore from './store/configureStore';
+import './index.css';
+// import App from './App';
+import AppRouter from "./routers/AppRouter"
+// import registerServiceWorker from './registerServiceWorker';
+
+const store = configureStore();
+const jsx = (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
+ReactDOM.render(jsx, document.getElementById('root'));
+// registerServiceWorker();
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Router, Switch} from 'react-router-dom';
+import PublicRoute from './PublicRoute';
+import createHistory from 'history/createBrowserHistory';
+
+import App2 from '../App2';
+import App from '../App';
+
+export const history = createHistory();
+
+const AppRouter = () => (
+  <Router history={history}>
+    <div>
+      <Switch>
+        <PublicRoute path="/app" component={App} exact={true} />
+        <PublicRoute path="/app2" component={App2} exact={true} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default  AppRouter;
