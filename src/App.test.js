f507dfbcb4ec9ce0fe493342f07dcdb0e8a67e0d
MarcelloVSilva <marcellovcs@gmail.com>
2018-09-26 23:53:15 -0300

    inicio firebase test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import firebase from 'firebase'
+import configForTest from './config.fb.test'
+import TestRenderer from 'react-test-renderer'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+
+// it.skip('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+it('Tests with firebase', async () => {
+  const fb = await firebase.initializeApp(configForTest);
+  var database = firebase.database.ref('/condominios/');
+  console.log(fb.name)
+  expect(database).toEqual('')
+  
+})
---
+++ src/config.fb.test.js
@@ -0,0 +1,9 @@
+export const configForTest = {
+    apiKey: "AIzaSyCAt4HeHsfpmYV_rOuRcebOqe8YxcQ8DqI",
+    authDomain: "cond-test-c04a0.firebaseapp.com",
+    databaseURL: "https://cond-test-c04a0.firebaseio.com",
+    projectId: "cond-test-c04a0",
+    storageBucket: "cond-test-c04a0.appspot.com",
+    messagingSenderId: "800660420561"
+};
+
